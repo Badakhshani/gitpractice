a06867ec390260516b9008956b70725e74260bc8
Mesbah <ainmes2017@gmail.com>
2023-12-29 10:30:55 -0500

    this the another commit

--- src/practicePackage/FirstClass.java
+++ src/practicePackage/FirstClass.java
@@ -5,6 +5,14 @@ public class FirstClass {
 	public static void main(String[] args) {
 	System.out.println("this is for test");
 
+	
+		
+		
 	}
-
+	
+public void test() {
+	System.out.println("this is the seonnd test ");
 }
+}
+
+
